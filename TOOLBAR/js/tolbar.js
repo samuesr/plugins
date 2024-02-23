632ce19f9c89f7dec3265443f209c25c41b4dab5
Samuel <samuel.sanram@educa.jcyl.es>
2024-02-23 17:40:20 +0100

    ampliacion toolbar

--- TOOLBAR/js/tolbar.js
+++ TOOLBAR/js/tolbar.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {  
 $('#boton').toolbar({content: '#tolbar',
-position: 'bottom',
-style: 'primary',
-event: 'click',
+position: 'bottom', //top, left, rigth, bottom
+style: 'primary', // ' ', primary, danger, warning, info, success, info-o, dark, ligth
+//animation: standard,flip, grow, flyin, bounce
+event: 'click', //hover
+//hideOnClick:true,
+//adjustment:35
 hideOnClick: true});
 
+$('#boton2').toolbar({content: '#tolbar2',
+position: 'left',
+style: 'danger',
+event: 'hover',
+animation: 'bounce',
+adjustment:60});
+
+$('#boton2').on('toolbarShown',mostar);
+
 })
+function mostar(e) {
+    console.log(e.target);
+}
